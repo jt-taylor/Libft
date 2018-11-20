e275134fe277c9376c5f05c39abf6923ffca7d05
Jackson Taylor <jtaylor@e1z2r5p11.42.us.org>
2018-11-19 16:10:45 -0800

    ft_lstdel was creating a corrupted pointer when called by fillit, im not sure why ?, think it was because it never looped and just called ft_lstdelone instead, now is in a while loop and uses del in self

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: jtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/21 16:35:07 by jtaylor           #+#    #+#             */
-/*   Updated: 2018/10/21 16:42:58 by jtaylor          ###   ########.fr       */
+/*   Updated: 2018/11/19 16:04:40 by jtaylor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	if ((*alst)->next)
-		ft_lstdel(&(*alst)->next, del);
-	ft_lstdelone(&(*alst), del);
+	if (*alst)
+	{
+		while (*alst)
+		{
+			del((*alst)->content, (*alst)->content_size);
+			free(*alst);
+			*alst = (*alst)->next;
+		}
+		*alst = NULL;
+	}
 }
